43263b33fa103ccc1a0028c562a3d3da646303f5
KarlsonJuy <kareolis@gmail.com>
2022-11-17 15:48:50 +1000

    initial

--- app.py
+++ app.py
@@ -3,31 +3,31 @@
 
 from dash import Dash, html, dcc
 import plotly.express as px
-import pandas as pd
+# import pandas as pd
 
 app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
     html.H1(children='Информационно аналитический центр'),
 
     html.Div(children='''
         Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    # '''),
+    #
+    # dcc.Graph(
+    #     id='example-graph',
+    #     figure=fig
+    # )
 ])
 
 if __name__ == '__main__':
